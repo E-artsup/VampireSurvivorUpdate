a4ba2790293457b0dd5f7a5ca76253d2096edd5c
Le_Thom <dev@lethom.fr>
2023-02-28 16:55:50 +0100

    Relocated PowerUtils.cs in ./Scripts/PowerManagement

--- VampireSurvivorUpdate/Assets/Scripts/PowersManagement/PowerUtils.cs
+++ VampireSurvivorUpdate/Assets/Scripts/PowersManagement/PowerUtils.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUtilsCS : MonoBehaviour
+public class PowerUtils : MonoBehaviour
 {
     
     // <summary>
