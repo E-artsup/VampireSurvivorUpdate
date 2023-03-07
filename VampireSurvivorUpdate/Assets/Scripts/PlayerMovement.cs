607a6221f5b282d44ba05fdb08cf4cb6a220154c
Alphadra46 <degrange.kevin@gmail.com>
2023-03-07 13:43:38 +0100

    Changing the axis of movement of the PlayerMovement and switch from 2D Physics components to 3D

--- VampireSurvivorUpdate/Assets/Scripts/PlayerMovement.cs
+++ VampireSurvivorUpdate/Assets/Scripts/PlayerMovement.cs
@@ -9,14 +9,14 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 2; //Could be placed in the Player Stats script if needed but this will need to be get here
 
     [Header("Private variables")]
-    private Rigidbody2D rb; //Used for the movement
+    private Rigidbody rb; //Used for the movement
     private float speedModifier = 1f; //Should be used for speed upgrades/modifiers instead of directly modifying the speed of the player. (If the speed variable is still in this script and not in the Player Stats)
     
     
     // Start is called before the first frame update
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -30,7 +30,7 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     private void Movement()
     {
-        rb.velocity = InputManager.instance.move.ReadValue<Vector2>().normalized * (speed * speedModifier);
+        rb.velocity = new Vector3(InputManager.instance.move.ReadValue<Vector2>().normalized.x * (speed * speedModifier),0,InputManager.instance.move.ReadValue<Vector2>().normalized.y * (speed * speedModifier));
     }
     
 }
